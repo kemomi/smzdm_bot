153804fbcfcd706d7a302c52779c9a5f17de37b2
kemomi <76780905+kemomi@users.noreply.github.com>
2022-08-26 10:08:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = 'qPb4XGwfcxClXsg7iQ4gBr2'
+TEST_COOKIE = '__ckguid=qPb4XGwfcxClXsg7iQ4gBr2; device_id=21307064331661477053101664d34df810b289d6fb695969b42111e185; homepage_sug=b; r_sort_type=score; __jsluid_s=10baee7fb8df534060998c45a072f907; _zdmA.vid=*; sajssdk_2015_cross_new_user=1; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1661477055; footer_floating_layer=0; ad_date=26; ad_json_feed=%7B%7D; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22182d7bfe8a36e4-020958dc1e4de3-72422f2d-1327104-182d7bfe8a4519%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fgithub.com%2Fstark666%2Fsmzdm_bot%22%7D%2C%22%24device_id%22%3A%22182d7bfe8a36e4-020958dc1e4de3-72422f2d-1327104-182d7bfe8a4519%22%7D; sess=BA-2e7MKVuFRVwUQTOG%2FqtBMnoZFhWIJdlR%2FTiOGNM8nI%2FukTjfLXp32Vgy4RO%2BAXq%2F0tVTVNYdDietkkJCNtB6bFmfwaiNfevK6ouu0dgiLckgBF700H0vzRXC; user=user%3A4367346541%7C4367346541; _zdmA.uid=ZDMA.JQpoLz0sJ.1661477092.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1661477092; smzdm_id=4367346541; bannerCounter=%5B%7B%22number%22%3A2%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A2%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; amvid=fbe094bdd562c118214957a2ce7eeb1a; _zdmA.time=1661477092556.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = '21307064331661477053101664d34df810b289d6fb695969b42111e185'
+SERVERCHAN_SECRETKEY = 'SCT3599TFdMPozRyT9USuYp6LrK8ZtOm'
